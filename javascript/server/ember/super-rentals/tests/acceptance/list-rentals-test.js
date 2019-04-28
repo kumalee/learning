d15badfe6cc44869de2912a4d894a180c552271a
Kuma Li <li.jun.kuma@gmail.com>
2019-04-28 16:12:33 +0800

    javascript/server/ember: Building a Complex Component ref: https://guides.emberjs.com/release/tutorial/autocomplete-component/

--- javascript/server/ember/super-rentals/tests/acceptance/list-rentals-test.js
+++ javascript/server/ember/super-rentals/tests/acceptance/list-rentals-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { click, visit, currentURL } from '@ember/test-helpers';
+import { click, visit, currentURL, fillIn, triggerKeyEvent } from '@ember/test-helpers';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { setupApplicationTest } from 'ember-qunit';
 
@@ -40,5 +40,13 @@ module('Acceptance | list rentals', function(hooks) {
   test('should list available rentals.', async function(assert) {
     await visit('/');
     assert.equal(this.element.querySelectorAll('.listing').length, 3, 'should display 3 listings');
+  });
+
+  test('should filter the list of rentals by city', async function(assert) {
+    await visit('/');
+    await fillIn('.list-filter input', 'seattle');
+    await triggerKeyEvent('.list-filter input', 'keyup', 69);
+    assert.equal(this.element.querySelectorAll('.results .listing').length, 1, 'should display 1 listing');
+    assert.ok(this.element.querySelector('.listing .location').textContent.includes('Seattle'), 'should contain 1 listing with location Seattle');
   });
 });
