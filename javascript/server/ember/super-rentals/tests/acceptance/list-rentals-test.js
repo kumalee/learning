36546b6c458d88cc47c0efc656a3f84bcb1833d7
Kuma Li <li.jun.kuma@gmail.com>
2019-04-28 17:01:39 +0800

    javascript/server/ember: Adding Nested Routes ref: https://guides.emberjs.com/release/tutorial/subroutes/

--- javascript/server/ember/super-rentals/tests/acceptance/list-rentals-test.js
+++ javascript/server/ember/super-rentals/tests/acceptance/list-rentals-test.js
@@ -60,4 +60,12 @@ module('Acceptance | list rentals', function(hooks) {
     assert.equal(this.element.querySelectorAll('.results .listing').length, 1, 'should display 1 listing');
     assert.ok(this.element.querySelector('.listing .location').textContent.includes('Seattle'), 'should contain 1 listing with location Seattle');
   });
+
+  test('should show details for a specific rental', async function(assert) {
+    await visit('/rentals');
+    await click(".grand-old-mansion");
+    assert.equal(currentURL(), '/rentals/grand-old-mansion', "should navigate to show route");
+    assert.ok(this.element.querySelector('.show-listing h2').textContent.includes("Grand Old Mansion"), 'should list rental title');
+    assert.ok(this.element.querySelector('.show-listing .description'), 'should list a description of the property');
+  });
 });
